c31bea4ab065b60b90e63542d29f231c67230639
dhanashriwayase2001 <102642323+dhanashriwayase2001@users.noreply.github.com>
2022-06-01 20:24:05 +0530

    Add files via upload

---
+++ todoList javascript.js
@@ -0,0 +1,34 @@
+function newTask()
+{
+document.getElementById('error').innerHTML="";
+
+	let name=document.getElementById('name').value;
+	if(name=='')
+    {
+		document.getElementById('error').innerHTML="Please Enter";
+	}
+    else
+    {
+				let box=document.getElementById('box');
+				let li=document.createElement('li');
+				li.textContent=name;
+
+				box.appendChild(li);
+
+				let a=document.createElement('a');
+				a.textContent='DELETE';
+				a.href="javascript:void(0)";
+
+				li.appendChild(a);
+
+
+	}
+		document.getElementById('name').value="";
+}
+
+let btn=document.querySelector('ul');
+btn.addEventListener("click",function(e){
+    let box=document.getElementById('box');
+    let li=e.target.parentNode;
+	box.removeChild(li);
+});
